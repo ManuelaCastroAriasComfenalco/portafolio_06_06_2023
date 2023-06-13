8daf9390ed03ac57013d53f6538c5447edcf71f3
ManuelaCastroAriasComfenalco <manuelacastro1705@gmail.com>
2023-06-13 12:35:39 -0500

    13/06/2023

--- js/main.js
+++ js/main.js
@@ -47,4 +47,9 @@ window.onscroll = () =>
     menuIcon.classList.remove('bx-x');
     navbar.classList.remove('active');
 
+    //animation footer on scroll
+    let footer = document.querySelector('footer');
+
+    footer.classList.toogle('show-animate', this.innerHeight + this.scrollY >= document.scrollingElement.
+    scrollHeight);
 }
