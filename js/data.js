ed4a15d45e784c45c6e683cd4209b97e0eb364f0
ManuelaCastroAriasComfenalco <manuelacastro1705@gmail.com>
2023-06-20 16:05:10 -0500

    json

--- js/data.js
+++ js/data.js
@@ -1,6 +1,13 @@
 addEventListener("DOMContentLoaded", async(e)=>
 {
     let data = await (await fetch("data.json")).json
-    console.log(data);
-      
+    
+    document.querySelector(".logo").insertAdjacentHTML("afterbegin", data.nombre);
+    document.querySelector(".navbar").insertAdjacentHTML("afterbegin", /*html*/`        
+        <a href="${data.menu[0].link}" class="active">${data.menu[0].nombre}</a>
+        <a href="${data.menu[1].link}">${data.menu[1].nombre}</a>
+        <a href="${data.menu[2].link}">${data.menu[2].nombre}</a>
+        <a href="${data.menu[3].link}">${data.menu[3].nombre}</a>
+        <a href="${data.menu[4].link}">${data.menu[4].nombre}</a>
+        `);
 })
